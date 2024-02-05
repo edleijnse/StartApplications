046526164740086c5848ce9bddea66f15874ef03
edleijnse <ed@leijnse.info>
2024-02-05 12:51:22 +0100

    Refactored with AI

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -3,147 +3,116 @@ Imports IniParser
 Imports IniParser.Model
 
 Public Class MainWindow
-    Dim ReadOnly parser = new FileIniDataParser()
+    Private ReadOnly parser As New FileIniDataParser()
 
     Public Sub New()
         InitializeComponent()
         AddHandler Me.Loaded, AddressOf Window_Loaded
-        Dim data As IniData
+        LoadButtonContentsFromIniFile()
+    End Sub
+
+    Private Sub LoadButtonContentsFromIniFile()
+        Dim filePath As String = GetIniFilePath()
         Try
-            Dim rootPath As String =
-                    New DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName
-            Dim filePath As String = Path.Combine(rootPath, "startapplications.ini")
-            data = parser.ReadFile(filePath)
-
-            ' If your ini file has a section named "Buttons"
-            Dim buttonSection = data("Buttons")
-
-            ' Assigns the content from your ini file to your buttons
-            Dim lastPartButton1 As String = Path.GetFileName(buttonSection("Button1"))
-            Dim lastPartButton2 As String = Path.GetFileName(buttonSection("Button2"))
-            Dim lastPartButton3 As String = Path.GetFileName(buttonSection("Button3"))
-            Dim lastPartButton4 As String = Path.GetFileName(buttonSection("Button4"))
-            Button1.Content = lastPartButton1
-            Button2.Content = lastPartButton2
-            Button3.Content = lastPartButton3
-            Button4.Content = lastPartButton4
-            ' ... repeat for every Button
-
-        Catch ex As FileNotFoundException
-            ' Handle the case when the ini file could not be found
-            MessageBox.Show(ex.Message)
+            If File.Exists(filePath) Then
+                Dim data = parser.ReadFile(filePath)
+                Dim buttonSection = data("Buttons")
+                AssignContentToButtons(buttonSection)
+            Else
+                MessageBox.Show($"File: {filePath} does not exist.")
+            End If
         Catch ex As Exception
-            ' Handle the case when the ini file could not be parsed correctly
             MessageBox.Show(ex.Message)
         End Try
     End Sub
 
-    Sub ChangeButtonContent(myButton as string, buttonContent as String)
+    Private Sub AssignContentToButtons(buttonSection As KeyDataCollection)
+        Button1.Content = Path.GetFileName(buttonSection("Button1"))
+        Button2.Content = Path.GetFileName(buttonSection("Button2"))
+        Button3.Content = Path.GetFileName(buttonSection("Button3"))
+        Button4.Content = Path.GetFileName(buttonSection("Button4"))
+    End Sub
 
-        Dim parser = New FileIniDataParser()
+    Private Function GetIniFilePath() As String
+        Dim rootPath As String = New DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName
+        Return Path.Combine(rootPath, "startapplications.ini")
+    End Function
 
+    Public Sub ChangeButtonContent(myButton As String, buttonContent As String)
+        Dim filePath As String = GetIniFilePath()
         Try
-            ' Read file
-            Dim rootPath As String =
-                    New DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName
-            Dim filePath As String = Path.Combine(rootPath, "startapplications.ini")
-
             If File.Exists(filePath) Then
                 Dim data = parser.ReadFile(filePath)
-
-                ' Modify content for Button
                 data("Buttons")(myButton) = buttonContent
-
-                ' Save file
                 parser.WriteFile(filePath, data)
             Else
                 MessageBox.Show($"File: {filePath} does not exist.")
             End If
-
-        Catch ex As FileNotFoundException
-            MessageBox.Show(ex.Message)
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
     End Sub
 
     Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
-        ' Position the window at the bottom of the working area (just above the taskbar )
         Me.Top = SystemParameters.WorkArea.Height - Me.Height
-        Me.Left = (SystemParameters.WorkArea.Width - Me.Width)/2
+        Me.Left = (SystemParameters.WorkArea.Width - Me.Width) / 2
     End Sub
 
     Private Sub Window_KeyDown(sender As Object, e As KeyEventArgs)
         Select Case e.Key
             Case Key.Left
-                Me.Left -= 200 'Moves the Window 10 units to the left.
+                Me.Left -= 200
             Case Key.Right
-                Me.Left += 200 'Moves the Window 10 units to the right.
+                Me.Left += 200
             Case Key.Up
-                Me.Top -= 200 'Moves the Window 10 units up.
+                Me.Top -= 200
             Case Key.Down
-                Me.Top += 200 'Moves the Window 10 units down.
+                Me.Top += 200
         End Select
     End Sub
 
     Private Sub BtnDropFile_Drop(sender As Object, e As DragEventArgs)
         Dim button = CType(sender, Button)
         If e.Data.GetDataPresent(DataFormats.FileDrop) Then
-            ' Note that you can have more than one file.
             Dim files = DirectCast(e.Data.GetData(DataFormats.FileDrop), String())
             If files IsNot Nothing AndAlso files.Length <> 0 Then
-                ' For this example, suppose we're only interested in the first file.
                 Dim lastPart As String = Path.GetFileName(files(0))
                 button.Content = lastPart
                 ChangeButtonContent(button.Name, files(0))
-                ' MessageBox.Show("You dropped: " & files(0))
             End If
         End If
     End Sub
 
     Private Sub ButtonClickHandler(sender As Object, e As RoutedEventArgs)
         Dim button = CType(sender, Button)
-        Dim saveContent = button.Content
-        if (InStr("Start Word", saveContent)) Then
-            Dim myApplicationHandler as New ApplicationHandler
-            myApplicationHandler.startWord()
-        Else if (InStr("Start Excel", saveContent)) Then
-            Dim myApplicationHandler as New ApplicationHandler
-            myApplicationHandler.startExcel()
-        Else if (InStr("Start OneNote", saveContent)) Then
-            Dim myApplicationHandler as New ApplicationHandler
-            myApplicationHandler.StartOneNote()
-        Else if (InStr("Start Outlook", saveContent)) Then
-            Dim myApplicationHandler as New ApplicationHandler
-            myApplicationHandler.StartOutlook()
-        Else
-            Dim myApplicationHandler as New ApplicationHandler
-            Dim parser = New FileIniDataParser()
-            Dim myApplication as string
+        Dim myApplicationHandler As New ApplicationHandler()
+        Select Case button.Content
+            Case "Start Word"
+                myApplicationHandler.startWord()
+            Case "Start Excel"
+                myApplicationHandler.startExcel()
+            Case "Start OneNote"
+                myApplicationHandler.StartOneNote()
+            Case "Start Outlook"
+                myApplicationHandler.StartOutlook()
+            Case Else
+                StartCustomApplication(button.Name, myApplicationHandler)
+        End Select
+    End Sub
 
-            Try
-                ' Read file
-                Dim rootPath As String =
-                        New DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName
-                Dim filePath As String = Path.Combine(rootPath, "startapplications.ini")
-
-                If File.Exists(filePath) Then
-                    Dim data = parser.ReadFile(filePath)
-
-                    ' Modify content for Button
-                    myApplication = data("Buttons")(button.Name)
-                Else
-                    MessageBox.Show($"File: {filePath} does not exist.")
-                End If
-
-            Catch ex As FileNotFoundException
-                MessageBox.Show(ex.Message)
-            Catch ex As Exception
-                MessageBox.Show(ex.Message)
-            End Try
-
-            myApplicationHandler.StartApplication(myApplication)
-        End If
+    Private Sub StartCustomApplication(buttonName As String, applicationHandler As ApplicationHandler)
+        Dim filePath As String = GetIniFilePath()
+        Try
+            If File.Exists(filePath) Then
+                Dim data = parser.ReadFile(filePath)
+                Dim myApplication = data("Buttons")(buttonName)
+                applicationHandler.StartApplication(myApplication)
+            Else
+                MessageBox.Show($"File: {filePath} does not exist.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
     End Sub
 
     Public Sub Button_Click(sender As Object, e As RoutedEventArgs)
